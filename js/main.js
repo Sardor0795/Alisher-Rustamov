ac98dea4ff1238ff1687ea29ea7389f992c1a8ef
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2022-08-26 18:50:03 +0500

    Make lang box closeable when clicks out of it

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 const elLangBox = document.querySelector(".js-lang-box");
 const elLangSelected = document.querySelector(".js-lang-selected");
+const elLangCloser = document.querySelector(".js-lang-closer");
+
+
 
 elLangSelected.addEventListener("click", function () {
   elLangBox.classList.toggle("open");
   elLangSelected.classList.toggle("open");
+  elLangCloser.classList.toggle("open");
 });
+
+elLangCloser.addEventListener("click", function () {
+    elLangBox.classList.remove("open");
+    elLangSelected.classList.remove("open");
+    elLangCloser.classList.remove("open");
+  });
