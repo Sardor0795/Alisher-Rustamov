b64685cb2ac7e3630de000765000196afc0104a9
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2022-08-26 18:56:41 +0500

    Make changeable site languages

--- js/main.js
+++ js/main.js
@@ -1,17 +1,35 @@
 const elLangBox = document.querySelector(".js-lang-box");
 const elLangSelected = document.querySelector(".js-lang-selected");
 const elLangCloser = document.querySelector(".js-lang-closer");
-
-
+const elLangUz = document.querySelector(".js-lang-uz");
+const elLangEn = document.querySelector(".js-lang-en");
 
 elLangSelected.addEventListener("click", function () {
-  elLangBox.classList.toggle("open");
-  elLangSelected.classList.toggle("open");
-  elLangCloser.classList.toggle("open");
+  toggle();
 });
 
 elLangCloser.addEventListener("click", function () {
-    elLangBox.classList.remove("open");
-    elLangSelected.classList.remove("open");
-    elLangCloser.classList.remove("open");
-  });
+  remove();
+});
+
+elLangEn.addEventListener("click", function () {
+  elLangSelected.textContent = "English";
+  remove();
+});
+
+elLangUz.addEventListener("click", function () {
+  elLangSelected.textContent = "O‘zbek";
+  remove();
+});
+
+function remove() {
+  elLangBox.classList.remove("open");
+  elLangSelected.classList.remove("open");
+  elLangCloser.classList.remove("open");
+}
+
+function toggle() {
+  elLangBox.classList.toggle("open");
+  elLangSelected.classList.toggle("open");
+  elLangCloser.classList.toggle("open");
+}
